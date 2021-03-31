ab149e70f7e8cdc2709eca1195e8b3e8dad293a7
Necritto <onetopyr@mail.ru>
2021-03-31 15:37:48 +0300

    Feature: general styles [ref:no-ref]

---
+++ src/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 3fr 1fr;
+  grid-gap: 15px;
+  color: #ffffff;
+`;
